1232b571eb013e399c9df8bb16287f7801b81fc1
James Devereux <devereuxjj@gmail.com>
2019-09-19 11:42:05 +0100

    moving root to home page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,9 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+ 
 
+  def self.generate_unsplash_photo
+    "https://source.unsplash.com/collection/2284026/1600x1200?#{rand(10000000000)}"
+  end
 end
